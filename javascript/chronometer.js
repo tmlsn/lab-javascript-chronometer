de5fa0494c03a9fd296c0908ef9d45c44f5e8dbc
tmlsn <theo.malless@gmail.com>
2021-11-27 19:23:28 +0100

    part one done

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,33 +1,54 @@
 class Chronometer {
+  
   constructor() {
+    this.currentTime = 0
+    this.intervalId = null
     // ... your code goes here
   }
 
   start(callback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      if(callback !== undefined) callback();
+      this.currentTime ++
+    }, 1000);
+  return this.intervalId
   }
 
   getMinutes() {
-    // ... your code goes here
+    let minutes = Math.floor(this.currentTime / 60)
+    return minutes
   }
 
   getSeconds() {
-    // ... your code goes here
+    // let seconds = this.currentTime - 60 * this.getMinutes()
+    let seconds = this.currentTime % 60 
+    return seconds
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    let valueString = `${value}`
+    if(valueString.length < 2) return `0${valueString}`
+    else return `${value}`
+    /* if(valueString.length > 2) return `${valueString[0]}${valueString[1]}` */
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId)
   }
 
   reset() {
+    this.currentTime = 0
+    this.intervalId = null
     // ... your code goes here
   }
 
   split() {
+    return `${this.computeTwoDigitNumber(this.getMinutes())}:${this.computeTwoDigitNumber(this.getSeconds())}`
+    /* let minutes = this.getMinutes()
+    let seconds = this.getSeconds()
+    let minutesFormat = this.computeTwoDigitNumber(minutes)
+    let secondsFormat = this.computeTwoDigitNumber(seconds)
+    return `${minutesFormat}:${secondsFormat}` */
     // ... your code goes here
   }
 }
